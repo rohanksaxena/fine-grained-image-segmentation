1ab40643d4a0c89a78420cfc4a4a6d9f05e0355c
rohanksaxena <rohansaxena3107@gmail.com>
2023-12-11 23:49:29 +0100

    Add infer script for ssn_dino

---
+++ infer_ssn_dino.py
@@ -0,0 +1,91 @@
+import math
+import numpy as np
+import torch
+
+from skimage.color import rgb2lab
+from skimage.segmentation._slic import _enforce_label_connectivity_cython
+
+from lib.ssn.ssn import sparse_ssn_iter
+
+
+@torch.no_grad()
+def inference(image, nspix, n_iter, fdim=None, color_scale=0.26, pos_scale=2.5, weight=None, enforce_connectivity=True):
+    """
+    generate superpixels
+
+    Args:
+        image: numpy.ndarray
+            An array of shape (h, w, c)
+        nspix: int
+            number of superpixels
+        n_iter: int
+            number of iterations
+        fdim (optional): int
+            feature dimension for supervised setting
+        color_scale: float
+            color channel factor
+        pos_scale: float
+            pixel coordinate factor
+        weight: state_dict
+            pretrained weight
+        enforce_connectivity: bool
+            if True, enforce superpixel connectivity in postprocessing
+
+    Return:
+        labels: numpy.ndarray
+            An array of shape (h, w)
+    """
+    from model import SSN_DINO
+    if weight is not None:
+        print("Loading model")
+        model = SSN_DINO(fdim, nspix, n_iter).to("cuda")
+        model.load_state_dict(torch.load(weight))
+        model.eval()
+    else:
+        model = SSN_DINO(fdim, nspix, n_iter).to("cuda")
+
+    height, width = image.shape[:2]
+
+    nspix_per_axis = int(math.sqrt(nspix))
+    pos_scale = pos_scale * max(nspix_per_axis/height, nspix_per_axis/width)
+
+    coords = torch.stack(torch.meshgrid(torch.arange(height, device="cuda"), torch.arange(width, device="cuda")), 0)
+    coords = coords[None].float()
+
+    image = rgb2lab(image)
+    image = torch.from_numpy(image).permute(2, 0, 1)[None].to("cuda").float()
+
+    Q, H, feats, num_spixels_width, num_spixels_height, tokens = model(color_scale*image, pos_scale*coords)
+
+    labels = H.reshape(height, width).to("cpu").detach().numpy()
+
+    if enforce_connectivity:
+        segment_size = height * width / nspix
+        min_size = int(0.06 * segment_size)
+        max_size = int(3.0 * segment_size)
+        labels = _enforce_label_connectivity_cython(
+            labels[None], min_size, max_size)[0]
+
+    return labels
+
+
+if __name__ == "__main__":
+    import time
+    import argparse
+    import matplotlib.pyplot as plt
+    from skimage.segmentation import mark_boundaries
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image", type=str, help="", default="Sooty_Albatross_0031_1066.jpg")
+    parser.add_argument("--weight", default="model_checkpoints/ssn_dino.pth", type=str, help="/path/to/pretrained_weight")
+    parser.add_argument("--fdim", default=20, type=int, help="embedding dimension")
+    parser.add_argument("--niter", default=10, type=int, help="number of iterations for differentiable SLIC")
+    parser.add_argument("--nspix", default=100, type=int, help="number of superpixels")
+    parser.add_argument("--color_scale", default=0.26, type=float)
+    parser.add_argument("--pos_scale", default=2.5, type=float)
+    args = parser.parse_args()
+
+    image = plt.imread(args.image)
+
+    s = time.time()
+    label = inference(image, args.nspix, args.niter, args.fdim, args.color_scale, args.pos_scale, args.weight)
+    plt.imsave("superpixel_segmentation.png", mark_boundaries(image, label))
